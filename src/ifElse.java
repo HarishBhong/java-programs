5109032b5e561e562c68fb576ea6866b9a9add18
HarishBhong <harishbhong@gmail.com>
2025-01-15 19:41:36 +0530

    if Else loop

---
+++ src/ifElse.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+public class ifElse
+{
+    public static void main(String[] args) {
+
+        Scanner var = new Scanner(System.in);
+
+        int day = var.nextInt();
+
+
+        if (day == 1){
+            System.out.println("Monday");
+        }
+        else if(day == 2){
+            System.out.println("Tuesday");
+        }
+        else if(day == 3){
+            System.out.println("wednesday");
+        }
+        else if(day == 4){
+            System.out.println("Thursday");
+        }
+        else if(day == 5){
+            System.out.println("FRIDAY");
+        }
+        else if(day == 6){
+            System.out.println("Saturday");
+        }
+    }
+}
